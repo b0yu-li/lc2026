5c2cdd8ead0d16f3e4e53275bd2d922eac60bd53
Boyu Li <lby1251853265@icloud.com>
2026-01-08 11:06:38 +0800

    [#1679] feat: waiting room (HashMap) analogy solution

--- src/main/java/org/boyu/interview/arrays/MaxNumberOfKSumPairs.java
+++ src/main/java/org/boyu/interview/arrays/MaxNumberOfKSumPairs.java
@@ -1,53 +1,26 @@
 package org.boyu.interview.arrays;
 
-import static java.util.stream.Collectors.groupingBy;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class MaxNumberOfKSumPairs {
     public int maxOperations(int[] nums, int k) {
-        final List<Integer> list = new ArrayList<>();
-        for (int num : nums) {
-            list.add(num);
-        }
+        // TODO: #1679 - Impl. the waiting room analogy
+        final Map<Integer, Integer> waitingRoom = new HashMap<>();
+        int count = 0;
 
-        int pairCounter = 0;
-
-        // TODO: #1679 - Refactor
-        final Map<Integer, Long> map = list.stream().collect(groupingBy(it -> it, Collectors.counting()));
-        for (Integer pairCandidate : map.keySet()) {
-            boolean amIDoneWithThisKey = false;
-
-            while (!amIDoneWithThisKey) {
-                final Long remainingLivesForCurrentCandidate = map.get(pairCandidate);
-                if (remainingLivesForCurrentCandidate < 1) {
-                    amIDoneWithThisKey = true;
-                    continue;
-                }
-                // tentatively remove this candidate, if fail to form a pair, revive it
-                map.put(pairCandidate, remainingLivesForCurrentCandidate - 1);
-
-                final Long supplePairLives = map.getOrDefault(k - pairCandidate, 0L);
-                final boolean foundAPair = supplePairLives >= 1;
-
-                if (foundAPair) {
-                    pairCounter++;
-                    map.put(k - pairCandidate, supplePairLives - 1);
-                } else {
-                    map.put(pairCandidate, remainingLivesForCurrentCandidate);
-                    amIDoneWithThisKey = true;
-                    continue;
-                }
-
-                if (map.get(pairCandidate) < 2) {
-                    amIDoneWithThisKey = true;
-                }
+        for (int currentGuest : nums) {
+            final int partner = k - currentGuest;
+            if (waitingRoom.containsKey(partner) && waitingRoom.get(partner) > 0) {
+                final Integer candidatePartnersCount = waitingRoom.get(partner);
+                waitingRoom.put(partner, candidatePartnersCount - 1);
+                count++;
+                continue;
             }
-
+            final Integer existingCountOfCurrentGuest = waitingRoom.getOrDefault(currentGuest, 0);
+            waitingRoom.put(currentGuest, existingCountOfCurrentGuest + 1);
         }
-        return pairCounter;
+
+        return count;
     }
 }
